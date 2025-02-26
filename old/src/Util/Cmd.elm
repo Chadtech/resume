7ad475d36074a3c7d12aaf1f6a3f51973c18e911
Chad Stearns <chad@superfocus.ai>
2025-02-26 00:34:00 -0500

    Deleting old

--- old/src/Util/Cmd.elm
+++
@@ -1,6 +0,0 @@
-module Util.Cmd exposing (withNoCmd)
-
-
-withNoCmd : model -> ( model, Cmd msg )
-withNoCmd model =
-    ( model, Cmd.none )
--- old/src/app.js
+++
@@ -1,6 +0,0 @@
-var app = Elm.Main.init();
-
-app.ports.downloadAsPdfInternal.subscribe(function(params){
-    window.open(params.url);
-})
-
